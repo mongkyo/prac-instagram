11eb37ac606a232385da9a1906a1fee1a0384334
JungMongKyo <dreamong91@gmail.com>
2018-10-18 21:26:41 +0900

    static()함수를 사용해서 MEDIA_URL, MEDIA_ROOT처리

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -115,8 +120,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
