70b5f5db6c09206e6af8782d961e23c8060a53b7
JungMongKyo <dreamong91@gmail.com>
2018-10-18 20:06:33 +0900

    Start practice instagram

---
+++ app/posts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    photo = models.ImageField(upload_to='post')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'post',
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
+    tags = models.ManyToManyField(
+        'HashTag',
+        blank=True,
+    )
+
+
+class HashTag(models.Model):
+    name = models.CharField('태그명', max_length=100)
+
+    def __str__(self):
+        return self.name
