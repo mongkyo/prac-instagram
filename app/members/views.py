c1f9d614aa7f25f8af652a5fd234e719634fb613
JungMongKyo <dreamong91@gmail.com>
2018-10-23 02:05:40 +0900

    login처리 및 base.html에서 로그인여부 버튼 표시

--- app/members/views.py
+++ app/members/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 from .forms import LoginForm
 
 
 def login_view(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('posts:post-list')
+        else:
+            pass
+
     else:
         form = LoginForm()
         context = {
