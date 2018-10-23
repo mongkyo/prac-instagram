cfaed61475c261c9a55fccd58af69725622092ba
JungMongKyo <dreamong91@gmail.com>
2018-10-23 10:15:35 +0900

    signup view에서 중복 render()부분 제거

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import LoginForm, SignupForm
@@ -30,11 +32,26 @@ def logout_view(request):
 
 
 def signup_view(request):
+
+    context = {
+        'form': SignupForm(),
+    }
     if request.method == 'POST':
-        pass
-    else:
-        form = SignupForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'members/signup.html', context)
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+
+        if User.objects.filter(username=username).exist():
+            context['error'] = f'사용자명 ({username})은 이미 사용중입니다.'
+        if password1 != password2:
+            context['error'] = '비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.'
+
+        else:
+            user = User.objects.create_user(
+                username=username,
+                password=password1,
+            )
+            login(request, user)
+            return redirect('posts:post-list')
+    return render(request, 'members/signup.html', context)
