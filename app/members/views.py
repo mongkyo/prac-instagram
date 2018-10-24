7c13e20db8ec514752d01571654cc96efe38c452
JungMongKyo <dreamong91@gmail.com>
2018-10-25 01:46:36 +0900

    데이터 유효성 검증을 제외한 Form사용

--- app/members/views.py
+++ app/members/views.py
@@ -33,24 +33,18 @@ def logout_view(request):
 
 def signup_view(request):
 
-    context = {
-        'form': SignupForm(),
-    }
+    context = {}
     if request.method == 'POST':
-        username = request.POST['username']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-
-        if User.objects.filter(username=username).exist():
-            context['error'] = f'사용자명 ({username})은 이미 사용중입니다.'
-        if password1 != password2:
-            context['error'] = '비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.'
-
-        else:
+        form = SignupForm(request.POST)
+        if form.is_valid():
             user = User.objects.create_user(
-                username=username,
-                password=password1,
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
             )
             login(request, user)
             return redirect('posts:post-list')
+    else:
+        form=SignupForm()
+
+    context['form']=form
     return render(request, 'members/signup.html', context)
