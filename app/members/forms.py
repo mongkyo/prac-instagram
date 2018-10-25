ea6a98d1c72dc9dec37de8246a34cbdf36c27818
JungMongKyo <dreamong91@gmail.com>
2018-10-25 10:03:49 +0900

    Bootstrap에 맞게 CSS동적 추가

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,9 +1,19 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 이 Form instance에 주어진 데이터가 유효하면
+        #  authenticate에서 리턴된 User객체를 채울 속성
+        self._user = None
+
     username = forms.CharField(
+        # 일반 input[type=text]
+        # form-control CSS클래스 사용
+        #  (Bootstrap규칙)
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
@@ -11,6 +21,7 @@ class LoginForm(forms.Form):
         )
     )
     password = forms.CharField(
+        # input[type=password]
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
@@ -18,9 +29,28 @@ class LoginForm(forms.Form):
         )
     )
 
+    def clean(self):
+        super().clean()
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise forms.ValidationError('사용자명 또는 비밀번호가 올바르지 않습니다')
+        self._user = user
+
+    @property
+    def user(self):
+        # 유효성 검증을 실행했을 때 (is_valid())
+        #  만약 필드나 폼에서 유효하지 않은 항목이 있다면
+        #  이 부분에 추가됨
+        if self.errors:
+            raise ValueError('폼의 데이터 유효성 검증에 실패하였습니다')
+        return self._user
+
 
 class SignupForm(forms.Form):
     username = forms.CharField(
+        label='사용자명',
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
@@ -28,6 +58,7 @@ class SignupForm(forms.Form):
         )
     )
     password1 = forms.CharField(
+        label='비밀번호',
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
@@ -35,6 +66,7 @@ class SignupForm(forms.Form):
         )
     )
     password2 = forms.CharField(
+        label='비밀번호 확인',
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
@@ -43,14 +75,27 @@ class SignupForm(forms.Form):
     )
 
     def clean_username(self):
+        # username이 유일한지 검사
         data = self.cleaned_data['username']
         if User.objects.filter(username=data).exists():
-            raise forms.ValidationError(f'이미 사용중인 사용자명입니다.')
+            self.fields['username'].widget.attrs['class'] += ' is-invalid'
+            raise forms.ValidationError('이미 사용중인 사용자명입니다')
         return data
 
-    def clean(self):
-        super().clean()
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
         if password1 != password2:
-            raise forms.ValidationError('비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다')
+            self.fields['password1'].widget.attrs['class'] += ' is-invalid'
+            self.fields['password2'].widget.attrs['class'] += ' is-invalid'
+            raise forms.ValidationError('비밀번호와 비밀번호 확인란의 값이 다릅니다')
+        return password2
+
+    def save(self):
+        if self.errors:
+            raise ValueError('폼의 데이터 유효성 검증에 실패했습니다')
+        user = User.objects.create_user(
+            username=self.cleaned_data['username'],
+            password=self.cleaned_data['password2'],
+        )
+        return user
